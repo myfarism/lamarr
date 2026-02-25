a097b5351b718caa99393ca3f69161a2611cae9e
myfarism <muhammad.farishafizh@student.upj.ac.id>
2026-02-25 12:09:57 +0700

    feat: enhance Firebase initialization to support credentials from environment variable

--- apps/api/pkg/firebase/firebase.go
+++ apps/api/pkg/firebase/firebase.go
@@ -2,7 +2,9 @@ package firebase
 
 import (
 	"context"
+	"encoding/base64"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
@@ -12,7 +14,21 @@ import (
 var AuthClient *auth.Client
 
 func Init() {
-	opt := option.WithCredentialsFile("firebase-service-account.json")
+	var opt option.ClientOption
+
+	// Production: pakai env var
+	// Development: pakai file JSON
+	if credBase64 := os.Getenv("FIREBASE_CREDENTIALS_BASE64"); credBase64 != "" {
+		credJSON, err := base64.StdEncoding.DecodeString(credBase64)
+		if err != nil {
+			log.Fatalf("Failed to decode Firebase credentials: %v", err)
+		}
+		opt = option.WithCredentialsJSON(credJSON)
+	} else if _, err := os.Stat("firebase-service-account.json"); err == nil {
+		opt = option.WithCredentialsFile("firebase-service-account.json")
+	} else {
+		log.Fatal("No Firebase credentials found. Set FIREBASE_CREDENTIALS_BASE64 or provide firebase-service-account.json")
+	}
 
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
